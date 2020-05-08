57969f11b595559109143b8886d57ca61ac61730
mielpops <zeitounamiel@MacBook-Pro-de-zeitoun.local>
2020-05-09 00:06:43 +0200

    app .js connexion port 3500

--- Node-liste-courses/app.js
+++ Node-liste-courses/app.js
@@ -1,9 +1,48 @@
+  
 const express = require('express');
-const router = express.Router();
+const app = express();
+const api = require('./api/v1/index');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
 
-router.get('/ping', (req, res) => {
-	res.status(200).json({ msg: 'pong', date: new Date()});
-});//localhost:3500/ping
+app.set('port', (process.env.port || 3500));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use('/api/v1',api);
 
 
-module.exports = router;
+mongoose.connect('mongodb://localhost:27017/listeCourse',{useNewUrlParser: true,useUnifiedTopology: true
+    
+});
+
+
+app.use((req, res) => {
+    const err = new Error('404 - Not found !!!!!');
+    err.status = 404;
+    res.json({msg : '404 - Not found !!!!!!!', err: err});
+     
+});
+
+
+
+connection
+
+    .on('error',(error) => {
+    console.warn('Erreur durant la connexion à MongoDb',error);
+    })
+    .once('open',() =>
+    {
+            console.log('La connexion MongoDb est établie');
+
+            app.listen(app.get('port'), () => {
+                console.log(`Express server listening on port !!! ${app.get('port')}`);
+            });
+        
+    })
+   
+
+
